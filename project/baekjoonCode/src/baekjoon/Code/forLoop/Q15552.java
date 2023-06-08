3b0fdd94adf6324ad5bf6280710e766744efb0ca
jaeheoon <aleh101@naver.com>
2023-06-08 20:46:18 +0900

    BaekJoon Code ForLoop add

--- project/baekjoonCode/src/baekjoon/Code/forLoop/Q15552.java
+++ project/baekjoonCode/src/baekjoon/Code/forLoop/Q15552.java
@@ -1,4 +1,4 @@
-package baekjoonCode;
+package baekjoon.Code.forLoop;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
-public class Main {
+public class Q15552 {
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
