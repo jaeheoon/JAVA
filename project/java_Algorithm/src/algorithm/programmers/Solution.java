8f56e2c945cccb98a3abd12a8fe319170eaa7252
jaeheoon <aleh101@naver.com>
2023-08-06 15:09:15 +0900

    DATA BackUp FIle

--- project/java_Algorithm/src/algorithm/programmers/Solution.java
+++ project/java_Algorithm/src/algorithm/programmers/Solution.java
@@ -1,28 +1,43 @@
 package algorithm.programmers;
 
+import java.util.Arrays;
+
 public class Solution {
-	public static int solution(int[] num_list) {
-		int odd = 0;
-		int even = 0;
-		for (int i = 0; i < num_list.length; i++) {
-			if (i % 2 == 1) {
-				odd += num_list[i];
-			} else {
-				even += num_list[i];
+	public static int[] solution(int[] arr, int divisor) {
+        int count = 0;
+        int number = 0;
+        for (int i : arr) {
+			if (i % divisor == 0 ) {
+				count++;
+			} 
+		}
+        
+        if(count == 0){
+            int[] answer = {-1};
+            return answer;
+        }
+        
+        int[] answer = new int[count];
+        for (int i = 0; i < arr.length; i++) {
+        	if (arr[i] % divisor == 0 ) {
+        		answer[number] = arr[i];
+        		number++;
 			}
 		}
-		if (odd > even) {
-			return odd;
-		} else {
-			return even;
-		}
-	}
+        Arrays.sort(answer);
+        return answer;
+    }
 
 	public static void main(String[] args) {
-		int[] array = {4, 2, 6, 1, 7, 6};
-		
-		int a = solution(array);
-		
-		System.out.println(a);
+		int[] array = {5, 9, 7, 10};
+//		long a = solution(121);
+//		boolean a = solution("Pyy");
+		int[] a = solution(array, 5);
+		for (int i : a) {
+			System.out.println(i);
+		}
+//		String[] array = {"jane", "kim"};
+// 		String a = solution(array);
+//		System.out.println(a);
 	}
 }
