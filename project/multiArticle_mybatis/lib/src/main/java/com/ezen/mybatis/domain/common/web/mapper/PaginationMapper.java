a2fedcdc1ffe587d402e6db044df37dc03828e33
jaeheoon <aleh101@naver.com>
2023-08-13 16:32:26 +0900

    계층형 게시판 및 페이징 처리를 위한 게시글 목록 출력

--- project/multiArticle_mybatis/lib/src/main/java/com/ezen/mybatis/domain/common/web/mapper/PaginationMapper.java
+++ project/multiArticle_mybatis/lib/src/main/java/com/ezen/mybatis/domain/common/web/mapper/PaginationMapper.java
@@ -15,4 +15,6 @@ public interface PaginationMapper {
 	/** 검색된 게시판 게시글 출력 */
 	public List<ArticleDTO> findSearchPage(Map<String, Object> searchArticle);
 
+	/** 계층형 게시판 및 페이징 처리를 위한 게시글목록 조회 */
+	public List<ArticleDTO> findSearchArticle(Map<String, Object> searchArticle);
 }
--- project/multiArticle_mybatis/lib/src/test/java/com/ezen/mybatis/page/PageTest.java
+++ project/multiArticle_mybatis/lib/src/test/java/com/ezen/mybatis/page/PageTest.java
@@ -80,13 +80,31 @@ public class PageTest {
 //		searchParams.put("value", 150);
 
 		searchParams.put("type", "name");
-		searchParams.put("value", "monday");
+		searchParams.put("value", "테스트");
 		
 		List<ArticleDTO> pages = mapper.findSearchPage(searchParams);
 		for (ArticleDTO articleDTO : pages) {
 			log.debug("검색된 페이지 정보 : {}", articleDTO);
 		}
 	}
+	
+	@Test
+	@DisplayName("계층형 게시판 및 페이징 처리를 위한 게시글 목록")
+//	@Disabled
+	void findSearchArticleTest() {
+		PaginationMapper mapper = sqlSession.getMapper(PaginationMapper.class);
+		
+		Map<String, Object> searchParams = new HashMap<String, Object>();
+		
+		searchParams.put("rows", 5);
+		searchParams.put("boardId", 20);
+		
+		List<ArticleDTO> pages = mapper.findSearchArticle(searchParams);
+		for (ArticleDTO articleDTO : pages) {
+			log.debug("검색된 계층형 페이지 정보 : {}", articleDTO);
+		}
+	}
+	
 
 	@AfterEach
 	public void destory() {
