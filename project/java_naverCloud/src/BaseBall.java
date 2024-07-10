21773ca5c7f713a0dbec8bf68125443d77761adc
jaeheoon <aleh101@naver.com>
2024-07-10 16:27:16 +0900

    야구 게임 프로그램 시작 로직 수정

--- project/java_naverCloud/src/BaseBall.java
+++ project/java_naverCloud/src/BaseBall.java
@@ -15,60 +15,70 @@ public class BaseBall {
 		int random = 0;
 		int som = 0;
 		Scanner sc = new Scanner(System.in);
-		
-		//난수 발생
-		for (int i = 0; i < com.length; i++) {
-			while (true) {
-				int count = 0;
-				random = (int) ((Math.random() * 9) + 1);
-				for (int j = 0; j < 3; j++) {
-					if (random == com[j]) count++;
-				}
-				if (count == 0) break;
-			}
-			com[i] = random;
-		}
-		for (int i = 0; i < com.length; i++) {
-			System.out.print(com[i] + "  ");
-		}
-		System.out.println();
+		String yn;
 		
 		while(true) {
-			int st = 0;
-			int ball = 0;
+			do {
+				System.out.print("게임을 실행하시겠습니까(Y/N) : ");
+				yn = sc.next();
+			} while (!yn.equals("Y") && !yn.equals("y") && !yn.equals("N") && !yn.equals("n"));
 			
-			//유저 입력값
-			while (true) {
-				int count = 0;
+			if(yn.equals("N") || yn.equals("n")) {
+				System.out.println("다음에 또 만나요~");
+				break;
+			}
+			//난수 발생
+			for (int i = 0; i < com.length; i++) {
+				while (true) {
+					int count = 0;
+					random = (int) ((Math.random() * 9) + 1);
+					for (int j = 0; j < 3; j++) {
+						if (random == com[j]) count++;
+					}
+					if (count == 0) break;
+				}
+				com[i] = random;
+			}
+			System.out.println();
+			
+			while(true) {
+				int st = 0;
+				int ball = 0;
 				
-				System.out.print("숫자 입력 : ");
-				som = sc.nextInt();
-				user[0] = som/100;
-				user[1] = (som/10)%10;
-				user[2] = som%10;
+				//유저 입력값
+				while (true) {
+					int count = 0;
+					
+					System.out.print("숫자 입력 : ");
+					som = sc.nextInt();
+					user[0] = som/100;
+					user[1] = (som/10)%10;
+					user[2] = som%10;
+					for (int i = 0; i < user.length; i++) {
+						for (int j = 0; j < user.length; j++) {
+							if (i != j) {
+								if(user[i] == user[j]) count++;
+							}
+						}
+					}
+					if(count == 0) break;
+					else System.out.println("중복되는 숫자가 있습니다.");
+				}
 				for (int i = 0; i < user.length; i++) {
-					for (int j = 0; j < user.length; j++) {
-						if (i != j) {
-							if(user[i] == user[j]) count++;
+					if (user[i] == com[i]) st++;
+					else {
+						for (int j = 0; j < user.length; j++) {
+							if(user[i] != com[i] && user[i] == com[j]) ball++;
 						}
 					}
 				}
-				if(count == 0) break;
-				else System.out.println("중복되는 숫자가 있습니다.");
-			}
-			for (int i = 0; i < user.length; i++) {
-				if (user[i] == com[i]) st++;
-				else {
-					for (int j = 0; j < user.length; j++) {
-						if(user[i] != com[i] && user[i] == com[j]) ball++;
-					}
+				System.out.println(st+"스트라이크 "+ball+"볼");
+				System.out.println();
+				if (st == 3) {
+					System.out.println("정답을 맞췄습니다. 프로그램을 종료합니다.");
+					System.out.println();
+					break;
 				}
-			}
-			System.out.println(st+"스트라이크 "+ball+"볼");
-			System.out.println();
-			if (st == 3) {
-				System.out.println("정답을 맞췄습니다. 프로그램을 종료합니다.");
-				break;
 			}
 		}
 		sc.close();
